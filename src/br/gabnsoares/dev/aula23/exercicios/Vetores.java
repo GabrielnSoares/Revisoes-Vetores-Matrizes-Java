ee75546ca281e351e01ed728988fed2b0dcbbbc1
Gabriel Soares <120689729+GabrielnSoares@users.noreply.github.com>
2025-01-17 23:24:14 -0300

    Melhorias exercicio vetores

--- src/br/gabnsoares/dev/aula23/exercicios/Vetores.java
+++ src/br/gabnsoares/dev/aula23/exercicios/Vetores.java
@@ -13,8 +13,8 @@ public class Vetores {
                 2 - REMOVER ITEM
                 3 - SAIR""");
 
-        String[] inventario = {" ",  " ", " "};
-        String opcao;
+        String[] inventario = {" ", " ", " "};
+        int opcao;
         int slot;
 
 
@@ -22,10 +22,11 @@ public class Vetores {
             System.out.println("SEU INVENTÁRIO: " + Arrays.toString(inventario));
             System.out.println("O QUE VOCÊ DESEJA FAZER?");
             Scanner sc = new Scanner(System.in);
-            opcao = sc.nextLine();
+            opcao = sc.nextInt();
+            sc.nextLine();
 
             switch (opcao) {
-                case "1":
+                case 1:
                     System.out.println("ONDE VOCÊ VAI GUARDAR O ITEM (INDICE)?");
                     slot = sc.nextInt();
                     sc.nextLine();
@@ -33,19 +34,20 @@ public class Vetores {
                     sc = new Scanner(System.in);
                     inventario[slot] = sc.nextLine();
                     break;
-                case "2":
+                case 2:
                     System.out.println("QUAL ITEM VOCÊ VAI REMOVER (INDICE)");
                     slot = sc.nextInt();
                     sc.nextLine();
                     inventario[slot] = null;
                     break;
-                case "3":
+                case 3:
+                    System.out.println("Game Over...");
                     break;
                 default:
                     System.out.println("DIGITE UMA OPÇÃO VÁLIDA!");
                     break;
             }
-        } while (!opcao.equalsIgnoreCase("3"));
+        } while (opcao != 3);
         System.out.println("Sistema Finalizado");
     }
 }
