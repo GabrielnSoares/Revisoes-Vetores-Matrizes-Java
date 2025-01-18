875d91ce2d9639436891608b482a903e4279f22c
Gabriel Soares <120689729+GabrielnSoares@users.noreply.github.com>
2025-01-17 23:18:25 -0300

    Vetores exercicio

---
+++ src/br/gabnsoares/dev/aula23/exercicios/Vetores.java
@@ -0,0 +1,51 @@
+package br.gabnsoares.dev.aula23.exercicios;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Vetores {
+
+    public static void main(String[] args) {
+
+        System.out.println("## GESTÃO DE INVENTARIO ##");
+        System.out.println("""
+                1 - ADICIONAR ITEM
+                2 - REMOVER ITEM
+                3 - SAIR""");
+
+        String[] inventario = {" ",  " ", " "};
+        String opcao;
+        int slot;
+
+
+        do {
+            System.out.println("SEU INVENTÁRIO: " + Arrays.toString(inventario));
+            System.out.println("O QUE VOCÊ DESEJA FAZER?");
+            Scanner sc = new Scanner(System.in);
+            opcao = sc.nextLine();
+
+            switch (opcao) {
+                case "1":
+                    System.out.println("ONDE VOCÊ VAI GUARDAR O ITEM (INDICE)?");
+                    slot = sc.nextInt();
+                    sc.nextLine();
+                    System.out.println("QUAL O NOME DO ITEM?");
+                    sc = new Scanner(System.in);
+                    inventario[slot] = sc.nextLine();
+                    break;
+                case "2":
+                    System.out.println("QUAL ITEM VOCÊ VAI REMOVER (INDICE)");
+                    slot = sc.nextInt();
+                    sc.nextLine();
+                    inventario[slot] = null;
+                    break;
+                case "3":
+                    break;
+                default:
+                    System.out.println("DIGITE UMA OPÇÃO VÁLIDA!");
+                    break;
+            }
+        } while (!opcao.equalsIgnoreCase("3"));
+        System.out.println("Sistema Finalizado");
+    }
+}
