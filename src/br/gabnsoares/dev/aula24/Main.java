f5e68b01961b768f9797e542cf2077a8c1dbc0fd
Gabriel Soares <120689729+GabrielnSoares@users.noreply.github.com>
2025-01-20 22:18:51 -0300

    For each

--- src/br/gabnsoares/dev/aula24/Main.java
+++ src/br/gabnsoares/dev/aula24/Main.java
@@ -1,8 +1,31 @@
 package br.gabnsoares.dev.aula24;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        System.out.println("Quantas notas você vai cadastrar?");
+
+        Scanner sc = new Scanner(System.in);
+        int qtdNotas = sc.nextInt();
+        sc.nextLine();
+
+        int[] notas = new int[qtdNotas];
+
+        for (int c = 0; c < qtdNotas; c++) {
+
+            System.out.println("Digite o valor da nota " + (c + 1) + ": ");
+
+            notas[c] = sc.nextInt();
+            sc.nextLine();
+        }
+
+        System.out.println("Imprimindo os valores das notas: ");
+        for (int item: notas) {
+            System.out.println(item);
+        }
 
     }
 }
