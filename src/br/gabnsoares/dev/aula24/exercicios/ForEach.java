ce11f1d218aa713ccffd84b282e7ceb890ce47e5
Gabriel Soares <120689729+GabrielnSoares@users.noreply.github.com>
2025-01-21 23:40:47 -0300

    For each exercicios

--- src/br/gabnsoares/dev/aula24/exercicios/ForEach.java
+++ src/br/gabnsoares/dev/aula24/exercicios/ForEach.java
@@ -6,28 +6,29 @@ public class ForEach {
 
     public static void main(String[] args) {
 
-        System.out.println("/** TODO LIST CLI * BY MICROSOFT **/");
+        System.out.println("/** TODO LIST CLI * BY MICROSOFT **\\");
         System.out.println("Quantas tarefas você deseja criar?");
 
         Scanner sc = new Scanner(System.in);
-        int qtdTask = sc.nextInt();
+        int qtdTarefa = sc.nextInt();
         sc.nextLine();
-
-        String[] tasks =  new String[qtdTask];
-        qtdTask =  Integer.parseInt(String.valueOf(qtdTask));
-        int c;
-
-        for (c = 0; c < qtdTask; c++) {
+// criando vetor do tomanho solicitado pelo client
+        String[] tarefas =  new String[qtdTarefa];
+        qtdTarefa =  Integer.parseInt(String.valueOf(qtdTarefa));
+// preencher cada tarefa individualmente
+        for (int c = 0; c < qtdTarefa; c++) {
 
             System.out.println("Digite a tarefa " +  (c+1) + ": ");
 
-            tasks[c] = sc.nextLine();
+            tarefas[c] = sc.nextLine();
         }
         System.out.println("CADASTRO ENCERRADO");
         System.out.println("Hoje você vai fazer as tarefas:");
-
-        for(String item: tasks) {
-            System.out.println(item);
+// for each - imprimir tarefas de forma ordenada
+        int c = 1;
+        for(String tarefa: tarefas) {
+            System.out.println(c + "-" + tarefa);
+            c++;
         }
 
     }
